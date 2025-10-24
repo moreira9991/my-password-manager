6841f7eb881a1f8d3ab2dee8af6b98bb35f2f282
moreira <gmoreira9991@gmail.com>
2025-10-24 18:41:21 +0100

    refactor: gui-layout and quality of life improvements

--- main.py
+++ main.py
@@ -45,35 +45,49 @@ if  mpawd== True:
 
 # Ver o porque de existir tantos self.result= True. penso tar a fazer nada - DONE
 
-# Sempre que abrir uma janela nova, impedir de interagir com janela pai enquanto janela inferior estiver aberta -
-# janelas =
-# Mypassword(parent->mypassword manager) - done
-# search (parent -> mypassword) - done
-# manage passwords ( parent -> mypassword) - eu quero que seja possivel interagir parcialmente, para poder copiar usernames e colar
-#tenho que desabilitar os outros botoes quando esta janela esta aberta. - parcialmente done
-# manage master password( parent -> mypasswrod) - done
+# Sempre que abrir uma janela nova, impedir de interagir com janela pai enquanto janela inferior estiver aberta - DONE
+#   janelas =
+#   Mypassword(parent->mypassword manager) - done
+#   search (parent -> mypassword) - done
+#   manage passwords ( parent -> mypassword) - eu quero que seja possivel interagir parcialmente, para poder copiar usernames e colar
+#   tenho que desabilitar os outros botoes quando esta janela esta aberta. - done
+#   manage master password( parent -> mypasswrod) - done
 # 
 
-# Implementar carregar "enter" ativa os botoes de entrar -
+# Implementar carregar "enter" ativa os botoes de entrar - DONE
+#   Masterpassword verify window - done
+#   Mypasswordmanager main window - done
+#   ManagePasswords search - done
+#   ManagePasswords managepwd - done
+
+
+# Focous em todos os textos ao abrir janela - DONE
 
 # Botao para fechar my passwords -??
 
-# Tirar a opção de redimensionar e full screen de maior parte das janelas, as que nao fazem sentido pelo menos - 
+# Tirar a opção de redimensionar e full screen de maior parte das janelas, as que nao fazem sentido pelo menos - DONE
 
 # Dimensionar janela de mypasswords para tamanho fixo, com botão de backup localizado canto inferior direito, e para ver as passwords
-# Ter uma maneira de dar scroll a todas as passwords que ficam fora do alcance da janela, ficando todas visiveis
+#   Ter uma maneira de dar scroll a todas as passwords que ficam fora do alcance da janela, ficando todas visiveis
+
+# Ao editar passwords ou master passwords, aparecer mensagem de erro se nada for alterado
 
 # encriptar json files -
 
 # nao deixar criar conta para mesmo site e mesmo username -
 
-# autocomplete ao escrever sites -
+# autocomplete ao escrever sites - talvez nao - 
 
 # organizar/refactor código -
 
+# comentar código em ingles
+
 # Arranjar maneira de fazer backup do ficheiro encryptado para email.
-# No canto inferior direito da janela "My Password's", ter um botao que,
-# ao providencionar master password, pede email e envia JSON file com as passwords encriptadas, com instruções de onde o path do ficheiro deve ficar  -
+#   No canto inferior direito da janela "My Password's", ter um botao que,
+#   ao providencionar master password, pede email e envia JSON file com as passwords encriptadas,
+#   com instruções de onde o path do ficheiro deve ficar  -
+
+# Script necessário para correr a app de maneira simples pelo terminal
 
 # readme -
 
